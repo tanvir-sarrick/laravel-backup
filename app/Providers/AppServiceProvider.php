8cea22d4894bc202858f875290d4a2866855d980
Tanvir Sarrick <84694368+tanvir-sarrick@users.noreply.github.com>
2024-07-14 17:43:40 +0600

    solved using GoogleDriveServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,38 +22,40 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-        $this->loadGoogleDriver();
+        // using loadGoogleDriver function
+        //$this->loadGoogleDriver();
+
+        // using Google
     }
 
-    private function loadGoogleDriver(){
-        try {
-            Storage::extend('google', function($app, $config) {
-                $options = [];
+    // private function loadGoogleDriver(){
+    //     try {
+    //         Storage::extend('google', function($app, $config) {
+    //             $options = [];
 
-                if (!empty($config['teamDriveId'] ?? null)) {
-                    $options['teamDriveId'] = $config['teamDriveId'];
-                }
+    //             if (!empty($config['teamDriveId'] ?? null)) {
+    //                 $options['teamDriveId'] = $config['teamDriveId'];
+    //             }
 
-                if (!empty($config['sharedFolderId'] ?? null)) {
-                    $options['sharedFolderId'] = $config['sharedFolderId'];
-                }
+    //             if (!empty($config['sharedFolderId'] ?? null)) {
+    //                 $options['sharedFolderId'] = $config['sharedFolderId'];
+    //             }
 
-                $client = new \Google\Client();
-                $client->setClientId($config['clientId']);
-                $client->setClientSecret($config['clientSecret']);
-                $client->refreshToken($config['refreshToken']);
+    //             $client = new \Google\Client();
+    //             $client->setClientId($config['clientId']);
+    //             $client->setClientSecret($config['clientSecret']);
+    //             $client->refreshToken($config['refreshToken']);
 
-                $service = new \Google\Service\Drive($client);
-                $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
-                $driver = new \League\Flysystem\Filesystem($adapter);
+    //             $service = new \Google\Service\Drive($client);
+    //             $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
+    //             $driver = new \League\Flysystem\Filesystem($adapter);
 
-                return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
-            });
-        } catch(\Exception $e) {
-            // your exception handling logic
+    //             return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
+    //         });
+    //     } catch(\Exception $e) {
+    //         // your exception handling logic
 
-            echo $e;
-        }
-    }
+    //         echo $e;
+    //     }
+    // }
 }
